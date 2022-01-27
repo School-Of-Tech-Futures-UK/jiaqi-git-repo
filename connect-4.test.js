ab4f47aee5b325ae37516fc677ab60a135a1a7d0
jiaqichen6688 <jiaqi.c.chen@accenture.com>
2022-01-27 21:12:32 +0000

    added tests for checkwinner

--- connect-4.test.js
+++ connect-4.test.js
@@ -1,5 +1,4 @@
 // Importing module
-const { redirect } = require('express/lib/response')
 const {
   checkWinner,
   resetGame,
@@ -10,51 +9,49 @@ const {
   gameState
 } = require('./connect-4.js')
 
-// Testing checkWinner Function
-// describe('When calling the checkWinner function', () => {
-// describe('CheckWinner will return either red, yellow, nobody or null when called', () => {
-//  scenarios = [
-//    [[
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      ['red', null, null, null, null, null, null],
-//      ['red', 'yellow', null, null, null, null, null]
-//    ], null],
-//    [[
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      ['red',null, null, null, null, null, null],
-//      ['red', null, null, null, null, null, null],
-//      ['red', null, null, null, null, null, null],
-//      ['red', null, null, null, null, null, null]
-//    ], 'red'],
-//    [[
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      [null, null, null, null, null, null, null],
-//      ['yellow', 'yellow', 'yellow', 'yellow', null, null, null]
-//    ], 'yellow'],
-//    [[
-//      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
-//      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow'],
-//      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
-//      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow'],
-//      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
-//      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow']
-//    ], 'nobody']
-//  ]
-//  it.each(scenarios)("when checkWinner is is called", (grid, expected) => {
-//    const actual = checkWinner(grid)
-//    expect(actual).toBe(expected)
-//  })
-// })
-// })
+ //Testing checkWinner Function
+let grid = []
 
-let grid =[]
+ describe('When calling the checkWinner function', () => {
+  scenarios = [
+    [[
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      ['red', null, null, null, null, null, null],
+      ['red', 'yellow', null, null, null, null, null]
+    ], null],
+    [[
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      ['red',null, null, null, null, null, null],
+      ['red', null, null, null, null, null, null],
+      ['red', null, null, null, null, null, null],
+      ['red', null, null, null, null, null, null]
+    ], 'red'],
+    [[
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      ['yellow', 'yellow', 'yellow', 'yellow', null, null, null]
+    ], 'yellow'],
+    [[
+      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
+      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow'],
+      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
+      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow'],
+      ['red', 'yellow', 'red', 'red', 'yellow', 'red', 'red'],
+      ['yellow', 'red', 'yellow', 'yellow', 'red', 'yellow', 'yellow']
+    ], 'nobody']
+  ]
+  it.each(scenarios)('CheckWinner will return either red, yellow, nobody or null when called', (grid, expected) => {
+    const actual = checkWinner(grid)
+    expect(actual).toBe(expected)
+  })
+ })
 
 describe('when 4 same coloured discs are placed consecutively in a row', () => {
   // arrange
@@ -136,6 +133,79 @@ describe('when 4 same coloured discs are placed consecutively in a column', () =
   })
 })
 
+describe('when 4 same coloured discs are placed consecutively in a diagonal', () => {
+  // arrange
+  let scenarios = []
+  let col = [0,1,2,3]
+  let grid = []
+  color=['red', 'yellow']
+  
+  function checkDiag(col, color) {
+    for (let i = 0; i < 3; i++) {
+      grid = [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ]
+      grid[i][col] = color
+      grid[i + 1][col + 1] = color
+      grid[i + 2][col + 2] = color
+      grid[i + 3][col + 3] = color
+      scenarios.push([grid, color])
+    }
+  }
+  
+  col.forEach( (col)=> {
+    checkDiag(col, color[0])
+  })
+  
+  col.forEach( (col)=> {
+    checkDiag(col, color[1])
+  })
+  it.each(scenarios)('checkWinner should return red or yellow ', (grid, expected) => {
+    const actual = checkWinner(grid)
+    expect(actual).toBe(expected)
+  })
+})
+
+
+describe('when 4 same coloured discs are placed consecutively in a reverse diagonal', () => {
+  // arrange
+  let scenarios = []
+  let col = [6,5,4,3]
+  let grid = []
+  color=['red', 'yellow']
+  function checkReverseDiag(col, color) {
+    for (let i = 0; i < 3; i++) {
+      grid = [
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null]
+      ]
+      grid[i][col] = color
+      grid[i + 1][col - 1] = color
+      grid[i + 2][col - 2] = color
+      grid[i + 3][col - 3] = color
+      scenarios.push([grid, color])
+    }
+  }
+  col.forEach( (col)=> {
+    checkReverseDiag(col, color[0])
+  })
+  col.forEach( (col)=> {
+    checkReverseDiag(col, color[1])
+  })
+  it.each(scenarios)('checkWinner should return red or yellow ', (grid, expected) => {
+    const actual = checkWinner(grid)
+    expect(actual).toBe(expected)
+  })
+})
 
 // Tests for takeTurn Function:
 describe('when takeTurn function is called', () => {
@@ -159,5 +229,36 @@ describe('when takeTurn function is called', () => {
     const expectedOutput ='red'
     const actualOutput = takeTurn(row, col)
     expect(actualOutput).toBe(expectedOutput)
+  })
+})
+
+// Test for getLowestAvailableRowInColumn function
+describe('When getLowestAvailableRowInColumn function is called', () => {
+  
+  beforeEach(() => {
+    grid = [
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null]
+    ]
+  })
+
+  test('should return 5', () => {
+    //  arrange
+    const pieces = ['yellow', 'yellow', 'yellow', 'red', 'red']
+    const ColumnNumber = 1
+    for (let i = 0; i < pieces.length; i++) {
+      pieces.forEach(piece => {
+        grid[i][ColumnNumber] = piece
+      })
+    }
+    // Act
+    const output = getLowestAvailableRowInColumn(ColumnNumber, grid)
+    const expectedOutput = 5
+    //  assert
+    expect(output).toBe(expectedOutput)
   })
 })
